1028a8dacccd0675fbfcdfba6a2dfd043cb80ca4
Boris Searles <boris@lucidgardens.com>
2012-02-21 16:27:41 +0100

    cleanup config options

--- release/jquery.flowgallery.js
+++ release/jquery.flowgallery.js
@@ -159,22 +159,22 @@ var FlowGallery = function(elem, options) {
 FlowGallery.defaults = {
   activeIndex: 0,          // index of image that is initially active
   animate: true,
-  circular: false,
-  enableKeyNavigation: true,   // enables forward/backward arrow keys for next/previous navigation
-  forwardOnActiveClick: false, // should clicking on active image, show next image?
-  forceWidth: false,
-  forceHeight: false,
   backgroundColor: 'black',
-  loadingWidth: 100,       // loading width to use if cannot be determined
-  loadingHeight: 60,       // loading height to use if cannot be determined
-  thumbWidth: 'auto',
-  thumbHeight: 'auto',
-  thumbTopOffset: 'auto',  // top offset in pixels or 'auto' for centering images within list height
-  imagePadding: 0,         // border of active image
-  thumbPadding: 0,         // border of thumbnails
-  loadingClass: "loading", // css class applied to <li> elements of loading images
+  circular: false,
+  duration: 900,            // animation duration (higher value = slower speed)
   easing: 'linear',
-  duration: 900            // animation duration (higher value = slower speed)
+  enableKeyNavigation: true,   // enables forward/backward arrow keys for next/previous navigation
+  forceHeight: false,
+  forceWidth: false,
+  forwardOnActiveClick: false, // should clicking on active image, show next image?
+  imagePadding: 0,         // border of active image
+  loadingClass: "loading", // css class applied to <li> elements of loading images
+  loadingHeight: 60,       // loading height to use if cannot be determined
+  loadingWidth: 100,       // loading width to use if cannot be determined
+  thumbHeight: 'auto',
+  thumbPadding: 0,         // border of thumbnails
+  thumbTopOffset: 'auto',  // top offset in pixels or 'auto' for centering images within list height
+  thumbWidth: 'auto'
 };
 
 
@@ -748,26 +748,7 @@ $.fn.flowgallery = function(options) {
 };
 
 // default options
-$.fn.flowgallery.defaults = {
-  activeIndex: 0,          // index of image that is initially active
-  animate: true,
-  circular: false,
-  enableKeyNavigation: true,   // enables forward/backward arrow keys for next/previous navigation
-  forwardOnActiveClick: false, // should clicking on active image, show next image?
-  forceWidth: false,
-  forceHeight: false,
-  backgroundColor: 'black',
-  loadingWidth: 100,       // loading width to use if cannot be determined
-  loadingHeight: 60,       // loading height to use if cannot be determined
-  thumbWidth: 'auto',
-  thumbHeight: 'auto',
-  thumbTopOffset: 'auto',  // top offset in pixels or 'auto' for centering images within list height
-  imagePadding: 0,         // border of active image
-  thumbPadding: 0,         // border of thumbnails
-  loadingClass: "loading", // css class applied to <li> elements of loading images
-  easing: 'linear',
-  duration: 900            // animation duration (higher value = slower speed)
-};
+$.fn.flowgallery.defaults = FlowGallery.defaults;
 
 
 })( jQuery, window , document );
--- src/plugin.js
+++ src/plugin.js
@@ -18,23 +18,4 @@ $.fn.flowgallery = function(options) {
 };
 
 // default options
-$.fn.flowgallery.defaults = {
-  activeIndex: 0,          // index of image that is initially active
-  animate: true,
-  circular: false,
-  enableKeyNavigation: true,   // enables forward/backward arrow keys for next/previous navigation
-  forwardOnActiveClick: false, // should clicking on active image, show next image?
-  forceWidth: false,
-  forceHeight: false,
-  backgroundColor: 'black',
-  loadingWidth: 100,       // loading width to use if cannot be determined
-  loadingHeight: 60,       // loading height to use if cannot be determined
-  thumbWidth: 'auto',
-  thumbHeight: 'auto',
-  thumbTopOffset: 'auto',  // top offset in pixels or 'auto' for centering images within list height
-  imagePadding: 0,         // border of active image
-  thumbPadding: 0,         // border of thumbnails
-  loadingClass: "loading", // css class applied to <li> elements of loading images
-  easing: 'linear',
-  duration: 900            // animation duration (higher value = slower speed)
-};
+$.fn.flowgallery.defaults = FlowGallery.defaults;
